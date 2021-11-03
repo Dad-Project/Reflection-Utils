830285b8c88c5acff9979117020d9da2f6986dda
Theo <Theo@DESKTOP-KAFEGGD>
2021-11-03 15:23:43 +0100

    First Commit

---
+++ src/main/java/fr/rowlaxx/genericutils/App.java
@@ -0,0 +1,13 @@
+package fr.rowlaxx.genericutils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
