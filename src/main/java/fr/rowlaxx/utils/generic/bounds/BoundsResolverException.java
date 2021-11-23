fbc504d5849f678972619ea95bf4025e7b5c221d
Theo <Theo@DESKTOP-SLV4CDM>
2021-11-23 02:02:41 +0100

    Added class header

--- src/main/java/fr/rowlaxx/utils/generic/bounds/BoundsResolverException.java
+++ src/main/java/fr/rowlaxx/utils/generic/bounds/BoundsResolverException.java
@@ -2,6 +2,16 @@ package fr.rowlaxx.utils.generic.bounds;
 
 import fr.rowlaxx.utils.generic.GenericUtilsException;
 
+/**
+ * Exception class for Bounds Resolver.
+ * This exception should not be thrown unless is an internal resolving problem occur.
+ * This include : 
+ * - Unknow Type
+ * - The impossibility to solve at least one TypeVariable
+ * @version 2021-11-23
+ * @author Theo
+ * @since 1.0.0
+ */
 public class BoundsResolverException extends GenericUtilsException {
 
 	private static final long serialVersionUID = -8324150669320629827L;
