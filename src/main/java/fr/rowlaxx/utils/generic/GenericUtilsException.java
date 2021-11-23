86a8d3b5806812758303edfb1d119c8b548b0cb0
Theo <Theo@DESKTOP-SLV4CDM>
2021-11-23 02:03:44 +0100

    Added class header

--- src/main/java/fr/rowlaxx/utils/generic/GenericUtilsException.java
+++ src/main/java/fr/rowlaxx/utils/generic/GenericUtilsException.java
@@ -1,5 +1,11 @@
 package fr.rowlaxx.utils.generic;
 
+/**
+ * Exception class for all the generic related stuff.
+ * @author Theo
+ * @since 1.0.0
+ * @version 2021-11-23
+ */
 public class GenericUtilsException extends RuntimeException {
 
 	private static final long serialVersionUID = -4173546150421668027L;
