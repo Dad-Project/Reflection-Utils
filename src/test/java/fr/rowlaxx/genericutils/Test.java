4ebc441c7f54114a72d8c855225a1aaefb3d3974
Theo <Theo@DESKTOP-SLV4CDM>
2021-11-23 01:46:00 +0100

    Moved test classes

--- src/test/java/fr/rowlaxx/genericutils/Test.java
+++ src/test/java/fr/rowlaxx/genericutils/Test.java
@@ -1,4 +1,4 @@
-package fr.rowlaxx.utils.generic;
+package fr.rowlaxx.genericutils;
 
 import java.lang.reflect.TypeVariable;
 import java.util.Arrays;
@@ -6,18 +6,18 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import fr.rowlaxx.utils.generic.bounds.BoundsResolver;
-import fr.rowlaxx.utils.generic.destination.DestinationResolverUtils;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		final Map<TypeVariable<?>,Class<?>[]> map = BoundsResolver.resolve(TestObject.Test2.class);
+final Map<TypeVariable<?>,Class<?>[]> map = BoundsResolver.resolve(TestObject.Test2.class);
 		
 		
 		System.out.println("Result : ");
 		for (Entry<TypeVariable<?>,Class<?>[]> entry : map.entrySet())
 			System.out.println(entry.getKey() + "\t" + Arrays.toString(entry.getValue()));
+		
 	}
 
 }
--- src/test/java/fr/rowlaxx/genericutils/TestObject.java
+++ src/test/java/fr/rowlaxx/genericutils/TestObject.java
@@ -1,13 +1,12 @@
-package fr.rowlaxx.utils.generic;
+package fr.rowlaxx.genericutils;
 
 import java.io.Serializable;
 import java.util.List;
 
-import javax.crypto.SecretKey;
 
 public class TestObject<J extends E, E extends Serializable, T extends J> {
 
-	class Test2<A extends List<? extends J>> {
+	class Test2<A extends List<? extends J[]>, K> {
 		
 		
 		
