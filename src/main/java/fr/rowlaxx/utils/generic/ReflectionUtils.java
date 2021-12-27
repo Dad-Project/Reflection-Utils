ad885827808c6c6ea09677447bb46b8b6435a0a7
RowlaxX <32131492+RowlaxX@users.noreply.github.com>
2021-12-27 12:34:26 +0100

    Small commit

---
+++ src/main/java/fr/rowlaxx/utils/generic/ReflectionUtils.java
@@ -0,0 +1,109 @@
+package fr.rowlaxx.utils.generic;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReflectionUtils {
+
+	public static Class<?> toWrapper(Class<?> clazz){
+		if (clazz == void.class)
+			return Void.class;
+		if (clazz == int.class)
+			return Integer.class;
+		if (clazz == byte.class)
+			return Byte.class;
+		if (clazz == short.class)
+			return Short.class;
+		if (clazz == long.class)
+			return Long.class;
+		if (clazz == float.class)
+			return Float.class;
+		if (clazz == double.class)
+			return Double.class;
+		if (clazz == boolean.class)
+			return Boolean.class;
+		return clazz;
+	}
+	
+	public static List<Method> getAllMethods(Class<?> clazz){
+		final List<Method> list = new ArrayList<>(10);
+
+		do {
+			for(Method e : clazz.getDeclaredMethods())
+				list.add(e);
+		}while ((clazz = clazz.getSuperclass()) != null);
+
+		return list;
+	}
+
+	public static List<Method> getAllMethods(Class<?> clazz, Class<? extends Annotation> annotation){
+		final List<Method> list = new ArrayList<>(10);
+
+		do {
+			for(Method e : clazz.getDeclaredMethods())
+				if (e.isAnnotationPresent(annotation))
+					list.add(e);
+		}while ((clazz = clazz.getSuperclass()) != null);
+
+		return list;
+	}
+	
+	public static List<Field> getAllFields(Class<?> clazz){
+		final List<Field> list = new ArrayList<>(10);
+
+		do {
+			for(Field e : clazz.getDeclaredFields())
+				list.add(e);
+		}while ((clazz = clazz.getSuperclass()) != null);
+
+		return list;
+	}
+	
+	public static List<Field> getAllFields(Class<?> clazz, Class<? extends Annotation> annotation){
+		final List<Field> list = new ArrayList<>(10);
+
+		do {
+			for(Field e : clazz.getDeclaredFields())
+				if(e.isAnnotationPresent(annotation))
+					list.add(e);
+		}while ((clazz = clazz.getSuperclass()) != null);
+
+		return list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T tryGet(Field field, Object accessor) {
+		try {
+			return (T) field.get(accessor);
+		}catch(IllegalAccessException e) {
+			return null;
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T tryInstanciate(Class<T> clazz, Object... params) {
+		for (Constructor<?> constructor : clazz.getConstructors()) {
+			if (constructor.getParameterCount() != params.length)
+				continue;
+			
+			try {
+				return (T) constructor.newInstance(params);
+			} catch (InstantiationException e) {
+				continue;
+			} catch (IllegalAccessException e) {
+				continue;
+			} catch (IllegalArgumentException e) {
+				continue;
+			} catch (InvocationTargetException e) {
+				continue;
+			}
+		}
+		return null;
+	}
+
+}
--- src/main/java/fr/rowlaxx/utils/generic/destination/Destination.java
+++ src/main/java/fr/rowlaxx/utils/generic/destination/Destination.java
@@ -1,15 +1,8 @@
 package fr.rowlaxx.utils.generic.destination;
 
-import java.io.Serializable;
-import java.lang.reflect.GenericDeclaration;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 public class Destination<T>  {
@@ -26,62 +19,116 @@ public class Destination<T>  {
 	public static final <T> Destination<T> from(Class<T> clazz, Destination<?>... generics){
 		return new Destination<T>(clazz, generics);
 	}
-
-	public static final <T> Destination<?>[] resolveClassBounds(Class<T> clazz) {
-		//On résoud d'abord le type raw
-		final TypeVariable<Class<T>>[] typeVariables = clazz.getTypeParameters();
-		final Map<TypeVariable<Class<T>>, Destination<?>> resolved = resolveRawType(clazz);
-		//TODO Résoudre les types génériques
-		
-		//On remet en ordre
-		final Destination<?>[] result = new Destination<?>[typeVariables.length];
-		for (int i = 0 ; i < result.length ; i++)
-			result[i] = resolved.get( typeVariables[i] );		
-		return result;
+	
+	public static final <T> Destination<T> parse(String string) throws ClassNotFoundException {
+		return parse(string, 0, string.length());
 	}
 	
-	/**
-	 * Résoud le raw type de tout les types variables d'une classe et et ses classes propriétaires si nécessaire
-	 * @param <T> Le type de classe
-	 * @param clazz La classe à résoudre 
-	 * @return une map contenant en clé le TypeVariable et en valeur la destination
-	 */
-	//TODO code cleanup
+	@SuppressWarnings("unchecked")
+	private static final <T> Destination<T> parse(String string, int start, int end) throws ClassNotFoundException {
+		final int startGeneric = string.indexOf('<', start);
+		if (startGeneric == -1)
+			return (Destination<T>) from(Class.forName(string));
+		
+		final String base = string.substring(start, startGeneric).trim();
+		final Builder<T> builder = (Builder<T>) newBuilder(Class.forName(base));
+		
+		int temp = startGeneric+1, temp2;
+		while(true) {
+			temp2 = next(string, temp);
+			if (temp2 > end || temp2 >= string.length()) {
+				builder.addGeneric(parse(string, temp, end));
+				break;
+			}
+			builder.addGeneric(parse(string, temp, temp2));
+			temp = temp2 + 1;
+		}
+		
+		return builder.build();
+	}
 	
+	private static final int next(String string, int start) {
+		int t = 0;
+		for (int i = start ; i < string.length() ; i++)
+		{
+			if (string.charAt(i) == '<')
+				t++;
+			if (string.charAt(i) == '>')
+				t--;
+			if (string.charAt(i) == ',' && t == 0)
+				return i;
+		}
+		return string.length();
+	}
 	
+	//Builders
+	public static <T> Builder<T> newBuilder(Class<T> clazz){
+		return new Builder<T>(clazz);
+	}
 	
-	
-	
-	
-	
+	public static class Builder<T> {
+		
+		//Variables
+		private final Class<T> clazz;
+		private List<Destination<?>> generics = null;
+		
+		//Constructeurs
+		private Builder(Class<T> clazz) {
+			this.clazz = Objects.requireNonNull(clazz, "clazz may not be null.");
+		}
+		
+		//Methodes
+		public Builder<T> addGeneric(Class<?> clazz) {
+			return addGeneric(from(clazz));
+		}
+		
+		public Builder<T> addGeneric(Destination<?> destination) {
+			Objects.requireNonNull(destination, "destination may not be null.");
+			if (getGenericCount() == getMaximumGenericCount())
+				throw new DestinationException("The class " + clazz + " only have " + getMaximumGenericCount() + " type parameters.");
+			if (generics == null)
+				this.generics = new ArrayList<>();
+			this.generics.add(destination);
+			return this;
+		}
+		
+		public Builder<T> addGeneric(String destination) throws ClassNotFoundException{
+			return addGeneric(parse(destination));
+		}
+		
+		public int getGenericCount() {
+			if (generics==null)
+				return 0;
+			return generics.size();
+		}
+		
+		public int getMaximumGenericCount() {
+			return clazz.getTypeParameters().length;
+		}
+		
+		public Destination<T> build(){
+			if (generics == null)
+				return new Destination<>(clazz);
+			
+			Destination<?>[] dest = new Destination<?>[generics.size()];
+			generics.toArray(dest);
+			return new Destination<T>(clazz, dest);
+		}
+	}
 	
 	//Variables
 	private final Class<T> clazz;
 	private final Destination<?>[] generics;
+	private final int genericsCount;
 
 	//Constructeurs
-	private Destination(final Class<T> clazz, boolean raw) {
+	private Destination(final Class<T> clazz) {
 		this.clazz = Objects.requireNonNull(clazz, "clazz may not be null.");
-		if (raw)
-			this.generics = null;
-		else {
-			final int length = clazz.getTypeParameters().length;
-			if (length == 0)
-				this.generics = null;
-			else
-				this.generics = new Destination[length];
-		}
-	}
-	
-	/**
-	 * Création d'une destination raw
-	 * @param clazz la classe
-	 */
-	public Destination(final Class<T> clazz){
-		this(clazz, true);
+		this.generics = null;
+		this.genericsCount = clazz.getTypeParameters().length;
 	}
 
-	public Destination(final Class<T> clazz, Class<?>... generics) {
+	private Destination(final Class<T> clazz, Class<?>... generics) {
 		this(clazz, toDestinationArray(generics));
 	}
 
@@ -92,44 +139,26 @@ public class Destination<T>  {
 		return array;
 	}
 
-	public Destination(final Class<T> clazz, Destination<?>... generics) {
+	private Destination(final Class<T> clazz, Destination<?>... generics) {
 		this.clazz = Objects.requireNonNull(clazz, "clazz may not be null.");
-		Objects.requireNonNull(generics, "generics may not be null.");
-
-		//On regarde s'il y a le bon nombre d'argument generique
-		final Destination<?>[][] expectedTypes = resolveClassBounds(clazz);
-
-		if (generics.length != expectedTypes.length)
-			throw new DestinationException("The class " + clazz + " must have " + expectedTypes.length + " generic types.");
+		this.genericsCount = clazz.getTypeParameters().length;
 		
-		this.generics = new Destination[expectedTypes.length];
-
-		Destination<?> current;
-		for (int i = 0 ; i < this.generics.length ; i++) {
-			current = generics[i];
-			Objects.requireNonNull(current, "array parameter generics has a null item at index " + i);
-			
-			for (Destination<?> expeted : expectedTypes[i])
-				if (!current.isInnerclass(expeted))
-					throw new DestinationException("The destination " + current + " must inherrit from " + expeted);
-			
-			this.generics[i] = generics[i];
+		if (generics == null)
+			this.generics = null;
+		else if (generics.length == genericsCount) {
+			this.generics = new Destination<?>[genericsCount];
+			for(int i = 0 ; i < genericsCount ; i++)
+				this.generics[i] = Objects.requireNonNull(generics[i], "generics[" + i + "] may not be null.");
 		}
+		else
+			throw new DestinationException("The class " + clazz + " must have " + genericsCount + " generic types.");
 	}
 
 	//Methodes
-	/**
-	 * Is this destination a raw type ?
-	 * Exemple :
-	 * String is a raw type
-	 * List is a raw type
-	 * List<String> is not a raw type
-	 * @return true if this destination is a raw type
-	 */
 	public final boolean isRaw() {
 		if (hasGenericParameters())
 			return false;
-		return clazz.getTypeParameters().length > 0;
+		return genericsCount > 0;
 	}
 	
 	public final Class<T> getDestinationClass() {
@@ -148,7 +177,7 @@ public class Destination<T>  {
 	
 	/**
 	 * This method return a copy of the internal array.
-	 * For optimization, stock the copy somewhere or use getGenericParameter(int index) and getGenericParametersCount()
+	 * For optimization, store the copy somewhere or use getGenericParameter(int index) and getGenericParametersCount()
 	 * @see getGenericParameter(int index)
 	 * @see getGenericParametersCount()
 	 * @return a copy of the generic parameter array. 
@@ -168,8 +197,6 @@ public class Destination<T>  {
 	}
 
 	public final boolean is(Class<?> clazz) {
-		if ( !isRaw() )
-			return false;
 		return this.clazz == clazz;
 	}
 
@@ -177,36 +204,8 @@ public class Destination<T>  {
 		return equals(dest);
 	}
 	
-	public final boolean isSuperclass(Class<?> clazz) {
-		if (clazz == null)
-			return false;
-		return clazz.isAssignableFrom(this.clazz);
-	}
-	
-	public final boolean isInnerclass(Class<?> clazz) {
-		if (clazz == null)
-			return false;
-		return this.clazz.isAssignableFrom(clazz);
-	}
-	
-	public final boolean isSuperclass(Destination<?> dest) {
-		if (!isSuperclass(dest.clazz))
-			return false;
-		//TODO checker les parametres generiques
-		return true;
-	}
-	
-	public final boolean isInnerclass(Destination<?> dest) {
-		if (dest == null)
-			return false;
-		return dest.isSuperclass(this);
-	}
-	
 	public final boolean isInstance(Object object) {
-		if (isRaw())
-			return this.clazz.isInstance(object);
-		//TODO vérifier les parametres generiques
-		return true;
+		return this.clazz.isInstance(object);
 	}
 
 	@Override
@@ -215,6 +214,7 @@ public class Destination<T>  {
 		int result = 1;
 		result = prime * result + ((clazz == null) ? 0 : clazz.hashCode());
 		result = prime * result + Arrays.hashCode(generics);
+		result = prime * result + genericsCount;
 		return result;
 	}
 
@@ -227,9 +227,14 @@ public class Destination<T>  {
 		if (getClass() != obj.getClass())
 			return false;
 		Destination<?> other = (Destination<?>) obj;
-		if (clazz != other.clazz)
+		if (clazz == null) {
+			if (other.clazz != null)
+				return false;
+		} else if (!clazz.equals(other.clazz))
 			return false;
 		if (!Arrays.equals(generics, other.generics))
+			return false;
+		if (genericsCount != other.genericsCount)
 			return false;
 		return true;
 	}
